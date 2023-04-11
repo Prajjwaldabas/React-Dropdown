081620d6b51cd4f948f0f217d909357f0f842aef
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-04-11 12:02:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import user from './assets/image/profile.jpg';
+
+
 
 function App() {
+  //using usestate to hide and show the dropdown 
+  const [open,setopen]=useState(false);
+
+
+// storing dropdown list items in an array
+  const Menus= ["Profile","Your Apps","Settings", "Logout"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='userImg'> 
+       
+      <img src={user} alt="user" onClick={()=>setopen(!open)} />
+
+    {/* checking if the dropdown state is false or open and if open then showing the dropdown list items */}
+     {
+      open && 
+      <div className='dropdown'>
+      <ul>
+        {
+          // mapping on the menus array to show the list items 
+          Menus.map((menu)=>(
+<li key={menu} onClick={()=>setopen(!open)}  > {menu} </li>
+          ))
+        }
+        
+      </ul>
+    </div>
+    
+     }
+     </div>
+     
+
+  
     </div>
   );
 }
